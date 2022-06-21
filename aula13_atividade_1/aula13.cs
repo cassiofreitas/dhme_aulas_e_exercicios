cd861e09d493f7912f31669df101cd135ddf7e05
Cássio Freitas <cgf04df@outlook.com>
2022-06-21 16:03:59 -0300

    Uploading aula13

---
+++ aula13_atividade_1/aula13.cs
@@ -0,0 +1,10 @@
+﻿namespace aula13_atividade_1
+{
+    internal class aula13
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
