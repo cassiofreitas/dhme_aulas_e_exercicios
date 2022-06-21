458955061cc5a59924cec187421af2e644da43a0
Cássio Freitas <cgf04df@outlook.com>
2022-06-21 16:53:26 -0300

    Publicando versão inicial com a primeira implementação.

--- aula13_atividade_1/Classes.cs
+++ aula13_atividade_1/Classes.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace aula13_atividade_1
 {
-    internal class Classes
+    public class Aluno
     {
+        public int Matricula;
+        public string Nome;
+        public string Sobrenome;
+        public int telefone;
+    }
+    internal class Escola
+    {
+        private Dictionary<int,Aluno> Matriculados = new Dictionary<int, Aluno>();
+        public void CadastrarAluno()
+        {
+            Aluno NossoAluno = new Aluno();
+            NossoAluno.Matricula = 1;
+            NossoAluno.Nome = "Maria";
+            NossoAluno.Sobrenome = "Jose";
+            NossoAluno.telefone = 99995555;
+
+            Matriculados.TryAdd(0, NossoAluno);
+        }
+        public void RemoverAluno()
+        {
+            Matriculados.Remove(0);
+        }
+        public void ListarMatriculas()
+        {
+
+        }
     }
 }
--- aula13_atividade_1/aula13.cs
+++ aula13_atividade_1/aula13.cs
@@ -1,9 +1,26 @@
-﻿namespace aula13_atividade_1
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula13_atividade_1
 {
+    enum CatalogoErros
+    {
+        OK = 200,
+        Redirecionamento_permanent = 301,
+        Redirecionamento_temporario = 302,
+        Nao_encontrado = 404,
+        Nao_autorizado = 405,
+        Servidor_indisponivel = 503,
+        Tempo_esgotado = 504
+    }
     internal class aula13
     {
         static void Main(string[] args)
         {
+            Escola.
             Console.WriteLine("Hello, World!");
         }
     }
