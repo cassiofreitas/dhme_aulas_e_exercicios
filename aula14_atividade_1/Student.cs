fecf5884f7e78964c7610c925c204da22d95da45
Luan Victor Leite da Silva <luan.silva@me.com.br>
2022-06-22 16:25:45 -0300

    Adicionando Atributos e Metodos no Student

--- aula14_atividade_1/Student.cs
+++ aula14_atividade_1/Student.cs
@@ -1,4 +1,33 @@
 namespace Aula14Atividade1;
 internal class Student
 {
+    public string Matricula { get; set; }
+    public string Nome { get; set; }
+    public string Sobrenome { get; set; }
+    public string Telefone { get; set; }
+    public Dictionary<int, double> Notas { get; set; } = new Dictionary<int, double> ();
+    private bool Status = true;
+
+    public void Atualizar_cadastro()
+    {
+
+    }
+    public void Registrar_nota()
+    {
+
+    }
+    public void Apresentar_notas()
+    { 
+    }
+    public void Estágio_do_aluno()
+    {
+        return Status;
+}
+    public void Média_aluno()
+    {
+         
+    }
+
+
+
 }
