87219b8446865ffa70000fe104f1faa2dd0c1b53
Luan Victor Leite da Silva <luan.silva@me.com.br>
2022-06-22 16:04:29 -0300

    Adicionando Atributos do Employee

--- aula14_atividade_1/Employee.cs
+++ aula14_atividade_1/Employee.cs
@@ -2,4 +2,16 @@ namespace Aula14Atividade1;
 
 internal class Employee
 {
+
+    public string Nome { get; set; }     
+    public string Sobrenome { get; set; }
+    public string Telefone { get; set; }
+    public decimal Salario { get; set; }
+    public string CPF { get; set; }
+    private bool Status = true;
+    // Estágio do Funcionário - Ativo ou Inativo (true/false).
+    public bool Estagio()
+    {
+        return Status;
+    }
 }
