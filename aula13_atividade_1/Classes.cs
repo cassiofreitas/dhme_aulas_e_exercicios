4ffb5abb1867a04e0ee8bf61968e64469b650fe6
Cássio Freitas <cgf04df@outlook.com>
2022-06-21 22:23:04 -0300

    Correção de Classe.cs; implementação da impressão de alunos da escola; implementação do programa principal para atender aos requisitos do exercício proposto

--- aula13_atividade_1/Classes.cs
+++ aula13_atividade_1/Classes.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace aula13_atividade_1
+﻿namespace aula13_atividade_1
 {
     public class Aluno
     {
@@ -13,10 +7,10 @@ namespace aula13_atividade_1
         public string? Sobrenome;
         public int Telefone;
     }
-    internal class Escola
+    public class Escola
     {
         private Dictionary<int,Aluno> Matriculados = new Dictionary<int, Aluno>();
-        public void CadastrarAluno(int NumeroMatricula, string? Nome, string? Sobrenome, int Telefone)
+        public bool CadastrarAluno(int NumeroMatricula, string? Nome, string? Sobrenome, int Telefone)
         {
             Aluno NossoAluno = new Aluno();
             NossoAluno.Matricula = NumeroMatricula;
@@ -24,15 +18,20 @@ namespace aula13_atividade_1
             NossoAluno.Sobrenome = Sobrenome;
             NossoAluno.Telefone = Telefone;
 
-            Matriculados.TryAdd(Matriculados.Count(), NossoAluno);
+            Matriculados.TryAdd(NumeroMatricula, NossoAluno);
+
+            return true;
         }
-        public void RemoverAluno()
+        public bool RemoverAluno(int NumeroMatricula)
         {
-            Matriculados.Remove(0);
+            return Matriculados.Remove(NumeroMatricula);
         }
         public void ListarMatriculas()
         {
-            Console.WriteLine(Matriculados[0].Nome);
+            foreach(KeyValuePair<int,Aluno> kvp in Matriculados)
+            {
+                Console.WriteLine($"Matrícula: {kvp.Value.Matricula} = {kvp.Value.Nome}");
+            }
         }
     }
 }
--- aula13_atividade_1/aula13.cs
+++ aula13_atividade_1/aula13.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿//Room 3 - 21/06/2022
+//
 namespace aula13_atividade_1
 {
     enum CatalogoErros
     {
         OK = 200,
-        Redirecionamento_permanent = 301,
+        Redirecionamento_permanente = 301,
         Redirecionamento_temporario = 302,
         Nao_encontrado = 404,
         Nao_autorizado = 405,
@@ -20,7 +16,33 @@ namespace aula13_atividade_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Escola NossaEscola = new Escola();
+            CatalogoErros RespostaDoServidor;
+
+            Console.Write("-[ Inserindo novos alunos ");
+            NossaEscola.CadastrarAluno(1,"Maria","Jose",555999);
+            NossaEscola.CadastrarAluno(2,"Jose","Maria",666888);
+            NossaEscola.CadastrarAluno(3, "Joaquim", "Couves", 777555);
+            NossaEscola.CadastrarAluno(4, "Pablo", "Picasso", 888444);
+            NossaEscola.CadastrarAluno(5, "Hugo", "Sanchez", 999333);
+            Console.WriteLine("]-");
+
+            Console.WriteLine("-[ Listando os alunos inseridos");
+            NossaEscola.ListarMatriculas();
+            Console.WriteLine("]-");
+
+            Console.Write("-[ Removendo aluno com Matrícula = 5: ");
+            Console.Write(NossaEscola.RemoverAluno(5));
+            Console.WriteLine(" ]-");
+
+            Console.WriteLine("-[ Listando os alunos após a remoção: ");
+            NossaEscola.ListarMatriculas();
+            Console.WriteLine("]-");
+
+            Console.WriteLine($"-[ Testando Catalogo de Erros para {CatalogoErros.Redirecionamento_permanente}");
+            RespostaDoServidor = CatalogoErros.Redirecionamento_permanente;
+            Console.Write($"Resposta do Servidor = {RespostaDoServidor}");
+            Console.WriteLine(" ]-");
         }
     }
 }
