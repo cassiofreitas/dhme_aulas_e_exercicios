520436a3d1f6f7d2eb2060f513e086ff0394f702
Kaoticz <1812311+Kaoticz@users.noreply.github.com>
2022-06-22 15:49:15 -0300

    Base implementation

--- aula14_atividade_1/Abstractions/Administration.cs
+++ aula14_atividade_1/Abstractions/Administration.cs
@@ -1,5 +1,18 @@
 namespace Aula14Atividade1.Abstractions;
 
-internal abstract class Administration
+public abstract class Administration<T>
 {
+    public abstract bool Register(T entity);
+
+    public abstract bool Remove(T entity);
+
+    public virtual bool Update(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual IReadOnlyList<T> List()
+    {
+        throw new NotImplementedException();
+    }
 }
--- aula14_atividade_1/Course.cs
+++ aula14_atividade_1/Course.cs
@@ -1,7 +1,26 @@
-using Aula14Atividade1.Abstracoes;
+using Aula14Atividade1.Abstractions;
 
 namespace Aula14Atividade1;
 
-internal class Course : Administration
+internal class Course : Administration<Student>
 {
+    public override bool Register(Student entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool Remove(Student entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool Update(Student entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IReadOnlyList<Student> List()
+    {
+        throw new NotImplementedException();
+    }
 }
