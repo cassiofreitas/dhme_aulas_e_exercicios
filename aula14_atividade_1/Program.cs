068fbba002abb7b72aa3067c6e1f0948e63c81fc
Cássio Freitas <cgf04df@outlook.com>
2022-06-22 20:56:28 -0300

    Montagem dos itens de ação em Program.cs (itens de menu)

--- aula14_atividade_1/Program.cs
+++ aula14_atividade_1/Program.cs
@@ -4,6 +4,19 @@ internal class Program
 {
     private static void Main()
     {
-        Console.WriteLine("Test Course and Company here.");
+        Console.WriteLine("(1) Cadastrar aluno");
+        Console.WriteLine("(3) Remover aluno");
+        Console.WriteLine("(5) Atualizar cadastro aluno");
+        Console.WriteLine("(7) Lançar notas de alunos");
+        Console.WriteLine("(8) Listar notas de alunos");
+        Console.WriteLine("(9) Listar média das notas de aluno");
+        Console.WriteLine("(10) Mostrar situação do estágio do aluno");
+
+        Console.WriteLine("(2) Cadastrar funcionário");
+        Console.WriteLine("(4) Remover funcionário");
+        Console.WriteLine("(6) Atualizar cadastro funcionário");
+        Console.WriteLine("(11) Mostrar situação do estágio do funcionário");
+
+        Console.WriteLine("(12) Listar todos os inscritos [ alunos e funcionários ]");
     }
 }
